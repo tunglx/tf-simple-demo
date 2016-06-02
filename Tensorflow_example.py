d4e1658d9ff5ffd1657f0b36d8a005abef42573a
tunglx_57 <tunglx_57@vnu.edu.vn>
2016-06-02 17:18:07 +0700

    add tensor board

--- Tensorflow_example.py
+++ Tensorflow_example.py
@@ -12,8 +12,16 @@ cost = tf.reduce_sum(tf.pow((y_ - y), 2))
 
 train_step = tf.train.GradientDescentOptimizer(0.00000001).minimize(cost)
 
+
 sess = tf.Session()
 init = tf.initialize_all_variables()
+
+W_hist = tf.histogram_summary("W", W)
+b_hist = tf.histogram_summary("b", b)
+
+summary_op = tf.merge_all_summaries()
+summary_writer = tf.train.SummaryWriter('/tmp/fc_logs', graph_def=sess.graph_def)
+
 sess.run(init)
 
 steps = 1000
@@ -22,6 +30,10 @@ for i in range(steps):
 	ys = np.array([[5*i]])
 	feed = {x: xs, y_: ys}
 	sess.run(train_step, feed_dict = feed)
+
+	summary_str = sess.run(summary_op)
+	summary_writer.add_summary(summary_str, i)
+
 	print("After %d iterations:" %i)
 	print("W: %f" % sess.run(W))
 	print("b: %f" % sess.run(b))
